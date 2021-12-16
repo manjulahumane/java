322b755548dc4dfa78483ffbec93aab966705b29
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-16 12:58:41 +0530

    uploading

---
+++ amount.java
@@ -0,0 +1,15 @@
+class amount{
+public static void main(String[] args){
+
+long[] amount={1111,2222,3333,444,3335,666,777,888,999,110};
+System.out.println(amount[0]);
+System.out.println(amount[1]);
+System.out.println(amount[2]);
+System.out.println(amount[3]);
+System.out.println(amount[4]);
+System.out.println(amount[5]);
+System.out.println(amount[6]);
+System.out.println(amount[8]);
+System.out.println(amount[9]);
+}
+}
